d9ef85dc4ec69efeb08790af461de537b105d24d
Ibrahim Munir-Zubair <im318@ic.ac.uk>
2019-12-16 21:41:49 +0000

    [IntCode] Refactor get and set memory values functionality. Added support for memory larger than program size.

--- intCode.py
+++ intCode.py
@@ -5,10 +5,9 @@ def get_modes(instr):
     a_mode = (instr % 1000)  // 100
     return (a_mode, b_mode, c_mode)
 
-
-
 class intCodeMachine:
     def __init__(self, program):
+        self.size = len(program)
         self.memory = list(x for x in program)
         self.pos = 0
         self.inputs = []
@@ -23,13 +22,25 @@ class intCodeMachine:
         c = (self.memory[self.pos+3] if c_mode == 1 else self.memory[self.memory[self.pos+3]]) if num >= 3 else None
         return (a, b, c)
 
+    def expand_memory(self, size):
+        to_add = size - len(self.memory)
+        for i in range(to_add): self.memory.append(0)
+        
+    def mem_get(self, position):
+        if position > len(self.memory): self.expand_memory(position+1)
+        return self.memory[position]
+
+    def mem_set(self, position, value):
+        if position > len(self.memory): self.expand_memory(position+1)
+        self.memory[position] = value
+        
     # Begin/Continue Running the machine
     def run(self, debug = False):
         if debug: print("Length", len(self.memory))
         
-        while (self.pos != len(self.memory) and self.memory[self.pos] != 99):
-            opcode = self.memory[self.pos] % 100
-            (a_mode, b_mode, c_mode) = get_modes(self.memory[self.pos])
+        while (self.pos != self.size and self.mem_get(self.pos) != 99):
+            opcode = self.mem_get(self.pos) % 100
+            (a_mode, b_mode, c_mode) = get_modes(self.mem_get(self.pos))
             modes = (a_mode, b_mode, 1)
 
             if opcode not in range(1,8+1):
@@ -38,12 +49,12 @@ class intCodeMachine:
 
             if (opcode == 1):
                 (a, b, c) = self.__get_ops(3, modes)
-                self.memory[c] = a + b
+                self.mem_set(c, a + b)
                 self.pos += 4
                 if debug: print("add", a, "+", b, "into", c)
             elif (opcode == 2):
                 (a, b, c) = self.__get_ops(3, modes)
-                self.memory[c] = a * b
+                self.mem_set(c, a * b)
                 self.pos += 4
                 if debug: print("mul", a, "*", b, "into", c)
             elif (opcode == 3):
@@ -52,7 +63,7 @@ class intCodeMachine:
                 if (len(self.inputs) == 0):
                     self.waitingForInput = True
                     return False
-                self.memory[a] = self.inputs.pop()
+                self.mem_set(a, self.inputs.pop())
                 self.pos += 2
                 if debug: print("input into", a)
             elif (opcode == 4):
@@ -71,12 +82,12 @@ class intCodeMachine:
                 if debug: print("jump zero to", b)
             elif (opcode == 7):
                 (a, b, c) = self.__get_ops(3, modes)
-                self.memory[c] = 1 if a < b else 0
+                self.mem_set(c, 1 if a < b else 0)
                 self.pos += 4
                 if debug: print("ly", a, "<", b, "into", c)
             elif (opcode == 8):
                 (a, b, c) = self.__get_ops(3, modes)
-                self.memory[c] = 1 if a == b else 0
+                self.mem_set(c, 1 if a == b else 0)
                 self.pos += 4
                 if debug: print("eq", a, "==", b, "into", c)
 
